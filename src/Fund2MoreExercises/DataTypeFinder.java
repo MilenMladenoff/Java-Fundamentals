ac92eec5b8a64c96176451bd8e05e029a125ee23
Milen Mladenov <93494386+MilenMladenoff@users.noreply.github.com>
2022-02-05 23:35:57 +0200

    Another shorter solution for DataTypeFinder.java

--- src/Fund2MoreExercises/DataTypeFinder.java
+++ src/Fund2MoreExercises/DataTypeFinder.java
@@ -6,89 +6,52 @@ public class DataTypeFinder {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-//        boolean isInteger = false;
-//        boolean isDouble = false;
+
 //        while (!input.equals("END")) {
-//            int decimalPointCounter = 0;
-//            if ((input.charAt(0) == '-' || input.charAt(0) == '+') && input.length() > 1) {
-//                for (int i = 1; i < input.length(); i++) {
-//                    isInteger = input.charAt(i) >= '0' && input.charAt(i) <= '9';
-//                    if (!isInteger) {
-//                        break;
-//                    }
-//                }
-//                for (int i = 1; i < input.length(); i++) {
-//                    isDouble = (input.charAt(i) >= '0' && input.charAt(i) <= '9');
-//                    if (input.charAt(i) == '.') {
-//                        decimalPointCounter++;
-//                        isDouble = true;
-//                    }
-//                    if (!isDouble || decimalPointCounter > 1) {
-//                        isDouble = false;
-//                        break;
-//                    }
-//                }
-//            } else {
-//                for (int i = 0; i < input.length(); i++) {
-//                    isInteger = input.charAt(i) >= '0' && input.charAt(i) <= '9';
-//                    if (!isInteger) {
-//                        break;
-//                    }
-//                }
-//                for (int i = 0; i < input.length(); i++) {
-//                    isDouble = (input.charAt(i) >= '0' && input.charAt(i) <= '9');
-//                    if (input.charAt(i) == '.') {
-//                        decimalPointCounter++;
-//                        isDouble = true;
-//                    }
-//                    if (!isDouble || decimalPointCounter > 1) {
-//                        isDouble = false;
-//                        break;
-//                    }
-//                }
-//            }
-//            if (isInteger) {
-//                System.out.printf("%s is integer type%n", input);
-//            } else if (isDouble) {
-//                System.out.printf("%s is floating point type%n", input);
-//            } else if (input.length() == 1) {
-//                System.out.printf("%s is character type%n", input);
-//            } else if (input.equalsIgnoreCase("true") || input.equalsIgnoreCase("false")) {
+//            if (input.equalsIgnoreCase("true") || input.equalsIgnoreCase("false")) {
 //                System.out.printf("%s is boolean type%n", input);
-//            } else {
+//                input = scan.nextLine();
+//                continue;
+//            }
+//            char firstSymbol = input.charAt(0);
+//            if (firstSymbol == 45 || firstSymbol == 43) {
+//                firstSymbol = input.charAt(1);
+//            }
+//            if ((firstSymbol < 48 || firstSymbol > 57) && input.length() == 1) {
+//                System.out.printf("%s is character type%n", input);
+//            }
+//            if ((firstSymbol < 48 || firstSymbol > 57) && input.length() > 1) {
 //                System.out.printf("%s is string type%n", input);
+//            }
+//            if (firstSymbol > 47 && firstSymbol < 58) {
+//                boolean isDouble = false;
+//                for (int i = 0; i < input.length(); i++) {
+//                    char symbol = input.charAt(i);
+//                    if (symbol == '.') {
+//                        System.out.printf("%s is floating point type%n", input);
+//                        isDouble = true;
+//                        break;
+//                    }
+//                }
+//                if (!isDouble) {
+//                    System.out.printf("%s is integer type%n", input);
+//                }
 //            }
 //            input = scan.nextLine();
 //        }
+
         while (!input.equals("END")) {
-            if (input.equalsIgnoreCase("true") || input.equalsIgnoreCase("false")) {
-                System.out.printf("%s is boolean type%n", input);
-                input = scan.nextLine();
-                continue;
-            }
-            char firstSymbol = input.charAt(0);
-            if (firstSymbol == 45 || firstSymbol == 43) {
-                firstSymbol = input.charAt(1);
-            }
-            if ((firstSymbol < 48 || firstSymbol > 57) && input.length() == 1) {
+            Scanner scan2 = new Scanner(input);
+            if (scan2.hasNextInt()) {
+                System.out.printf("%s is integer type%n", input);
+            } else if (scan2.hasNextDouble()) {
+                System.out.printf("%s is floating point type%n", input);
+            } else if (input.length() == 1) {
                 System.out.printf("%s is character type%n", input);
-            }
-            if ((firstSymbol < 48 || firstSymbol > 57) && input.length() > 1) {
+            } else if (scan2.hasNextBoolean()) {
+                System.out.printf("%s is boolean type%n", input);
+            } else if (scan2.hasNextLine()) {
                 System.out.printf("%s is string type%n", input);
-            }
-            if (firstSymbol > 47 && firstSymbol < 58) {
-                boolean isDouble = false;
-                for (int i = 0; i < input.length(); i++) {
-                    char symbol = input.charAt(i);
-                    if (symbol == '.') {
-                        System.out.printf("%s is floating point type%n", input);
-                        isDouble = true;
-                        break;
-                    }
-                }
-                if (!isDouble) {
-                    System.out.printf("%s is integer type%n", input);
-                }
             }
             input = scan.nextLine();
         }
